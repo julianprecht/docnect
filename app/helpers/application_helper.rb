eb26e6c5c22e0ba6c980751c5a5fecd4b45a0aeb
Alex Macpherson <alexjamesmacpherson@gmail.com>
2017-02-20 18:43:24 +0000

    Static pages, basic header/footer and app styling, other minor changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Define title of page, must assert <% provide(:title, "Home") %> in all views
+  def full_title(page_title = '')
+    base_title = 'Docnect'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
