3e73f0b64debc07602a0e38cf7331063f2e8e4fb
Alex Macpherson <alexjamesmacpherson@gmail.com>
2017-02-25 14:16:01 +0000

    Heroku hotfix

--- db/migrate/20170224172428_add_question_to_users.rb
+++ db/migrate/20170224172428_add_question_to_users.rb
@@ -1,6 +1,6 @@
 class AddQuestionToUsers < ActiveRecord::Migration[5.0]
   def change
     add_reference :users, :questions, default: 1
-    add_foreign_key :users, :questions
+    add_foreign_key :users, :questions, column: :questions_id
   end
 end
